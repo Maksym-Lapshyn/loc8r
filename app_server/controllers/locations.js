6e44c3d7676b17c77a8076594d7f1494887761a9
Maksym <maksym.lapshyn@gmail.com>
2017-11-29 15:04:17 +0200

    Added views with static content.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 module.exports.homeList = function(req,res){
-    res.render('locations-list', {title:'Home'});
+  res.render('locations-list', {title:'Home'});
 };
 
 module.exports.locationInfo = function(req,res){
-    res.render('location-info', {title:'Location info'});
+  res.render('location-info', {title:'Location info'});
 };
 
 module.exports.newReview = function(req,res){
-    res.render('new-review', {title:'New review'});
+  res.render('location-review-form', {title:'New review'});
 };
