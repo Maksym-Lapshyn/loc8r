a11e96a8fdabaa896c21c129b3ca301c094c4738
Maksym Lapshyn <Maksym_Lapshyn@epam.com>
2017-12-08 17:05:54 +0200

    Added mongodb.

---
+++ app_server/models/db.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+require('./locations');
+
+var dbUri = 'mongodb://localhost/Loc8r';
+var shutdown;
+
+mongoose.connect(dbUri, {
+    useMongoClient: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ' + dbUri);
+});
+
+mongoose.connection.on('error', err => {
+    console.log("Mongoose connection error " + err)
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+shutdown = function(msg, callback){
+    mongoose.connection.close(() => {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    shutdown('Nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', () => {
+    shutdown('App termination', () => {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+    shutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
